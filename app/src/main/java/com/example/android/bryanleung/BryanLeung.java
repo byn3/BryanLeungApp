46a02986b9407725a692ed96f86ada0386f2dab4
byn3 <bryanleung92@gmail.com>
2017-07-21 14:56:08 -0700

    yo
    dtyty

---
+++ app/src/main/java/com/example/android/bryanleung/BryanLeung.java
@@ -0,0 +1,36 @@
+package com.example.android.bryanleung;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import static com.example.android.bryanleung.R.layout.bryanleung;
+import static java.security.AccessController.getContext;
+
+public class BryanLeung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(bryanleung);
+
+        //makes a welcome message
+        //change and edit so nor hard stringed or whatnot
+        Toast.makeText(BryanLeung.this,"You so dam sexy and awesome", Toast.LENGTH_SHORT).show();
+    }
+    public void AboutMe(View view){
+        Intent i=new Intent(this, AboutMe.class);
+        startActivity(i);
+    }
+    public void ContactSocial(View view){
+        Intent i=new Intent(this, ContactSocial.class);
+        startActivity(i);
+    }
+    public void PlacesVisited(View view){
+        Intent i=new Intent(this, PlacesVisited.class);
+        startActivity(i);
+    }
+
+}
